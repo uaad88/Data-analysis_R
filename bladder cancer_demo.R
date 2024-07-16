e387644242e3cdd4fb4196d7457eb388a04f2587
You-Jun Lai <69507788+uaad88@users.noreply.github.com>
2024-07-17 00:48:08 +0800

    Update bladder cancer_demo.R

--- bladder cancer_demo.R
+++ bladder cancer_demo.R
@@ -2,7 +2,6 @@
 library(glmnet)
 library(readxl)
 library(tidyverse)
-library(ggplot2)
 #-----------------(2.input data)--------------------
 Resource: https://www.cbioportal.org/
 Type_of_cancer: bladder
@@ -10,11 +9,69 @@ Cancer_study_identifier: paired_bladder_2022
 Name: Bladder Cancer (MSK, Cell Reports 2022)
 Description: Targeted sequencing of 1659 Bladder Cancer tumor/normal samples via MSK-IMPACT.
 Short_name: BLADDER (MSK 2022)
-#-----------------(3.simple logistic regression)--------------------------------
+
+#-----------------(3.data cleaning)---------------------------------------------
+#race
+bc_03[bc_03$Race.Category =="ASIAN-FAR EAST/INDIAN SUBCONT", c("Race_01")] <- 0 #others
+bc_03[bc_03$Race.Category  =="NO VALUE ENTERED", c("Race_01")] <- 0 #others
+bc_03[bc_03$Race.Category  =="OTHER", c("Race_01")] <- 0 #others
+bc_03[bc_03$Race.Category  =="PT REFUSED TO ANSWER", c("Race_01")] <- 0 #others
+bc_03[bc_03$Race.Category =="RACE", c("Race_01")] <- 0 #others
+bc_03[bc_03$Race.Category =="UNKNOWN", c("Race_01")] <- 0 #others
+bc_03[bc_03$Race.Category =="ASIAN-FAR EAST/INDIAN SUBCONT", c("Race_01")] <- 1
+bc_03[bc_03$Race.Category  =="BLACK OR AFRICAN AMERICAN", c("Race_01")] <- 2
+bc_03[bc_03$Race.Category  =="NATIVE AMERICAN-AM IND/ALASKA", c("Race_01")] <- 3
+bc_03[bc_03$Race.Category  =="ASIAN-FAR EAST/INDIAN SUBCONT", c("Race_01")] <- 4
+bc_03[bc_03$Race.Category =="WHITE", c("Race_01")] <- 5
+
+#sex
+bc_03[bc_03$Sex =="Male", c("sex_01")] <- 1
+bc_03[bc_03$Sex =="Female", c("sex_01")] <- 0
+
+#Ethnicity.Category
+bc_03[bc_03$Ethnicity.Category =="", c("Ethnicity_01")] <- 0
+bc_03[bc_03$Ethnicity.Category =="Cuban", c("Ethnicity_01")] <- 1
+bc_03[bc_03$Ethnicity.Category =="ETHNICITY", c("Ethnicity_01")] <- 2
+bc_03[bc_03$Ethnicity.Category =="Mexican (includes Chicano)", c("Ethnicity_01")] <- 3
+bc_03[bc_03$Ethnicity.Category =="Non-Spanish; Non-Hispanic", c("Ethnicity_01")] <- 4
+bc_03[bc_03$Ethnicity.Category =="Puerto Rican", c("Ethnicity_01")] <- 5
+bc_03[bc_03$Ethnicity.Category =="South/Central America (except Brazil)", c("Ethnicity_01")] <- 6
+bc_03[bc_03$Ethnicity.Category =="Spanish  NOS; Hispanic NOS, Latino NOS", c("Ethnicity_01")] <- 7
+bc_03[bc_03$Ethnicity.Category =="Unknown whether Spanish or not", c("Ethnicity_01")] <- 8
+
+#overall survival status
+bc_03[bc_03$Overall.Survival.Status =="", c("OS.status_01")] <- NA
+bc_03[bc_03$Overall.Survival.Status =="LIVING", c("OS.status_01")] <- 0
+bc_03[bc_03$Overall.Survival.Status =="DECEASED", c("OS.status_01")] <- 1
+
+#overall survival time
+bc_03$OS_time<-as.numeric(bc_03$Overall.Survival..Months.)
+
+#smoker
+bc_03[bc_03$Smoker=="Never", c("smoker_01")] <- 0
+bc_03[bc_03$Smoker=="Former", c("smoker_01")] <- 1
+bc_03[bc_03$Smoker=="Active", c("smoker_01")] <- 2
+
+#age at diagnosis
+bc_03$age_01<-as.numeric(bc_03$Age.at.Diagnosis)
+
+#MSI score
+bc_03$MSI_01<-as.numeric(bc_03$MSI.Score)
+
+#TMB
+bc_03$TMB_01<-as.numeric(bc_03$TMB..nonsynonymous.)
+
+#final
+bc_04<-bc_03[,c(1,14:22)]
+
+#delete na
+bc_05<-na.omit(bc_04)
+
+#-----------------(4.simple logistic regression)--------------------------------
 #what is the different risk between female and male in the bladder cancer?
 s_glm = glm(sex_01 ~ smoker_01, data = bc_05, family = "binomial")
 
-#-----------------(4.prediction models)-----------------------------------------
+#-----------------(5.prediction models)-----------------------------------------
 #Reference: https://glmnet.stanford.edu/articles/glmnet.html
 #what is the best clinical model ? Ans:ROC prediction for logistic regression
 vari<-data.matrix(bc_05) 
